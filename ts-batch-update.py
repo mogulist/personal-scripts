50ebb5876f4f24d9efd3ac21deeb61eae480f23b
Herbert Lim <mogulist@naver.com>
2025-04-20 17:50:30 +0900

    PRO_VID 로 시작하는 파일의 시간도 업데이트 함

--- ts-batch-update.py
+++ ts-batch-update.py
@@ -6,9 +6,14 @@ from pathlib import Path
 def update_file_timestamp(file_path):
     # 파일 이름에서 날짜와 시간 추출
     file_name = os.path.basename(file_path)
-    date_time_str = file_name.split('_')[1] + file_name.split('_')[2]
     
-    # 날짜와 시간 파싱
+    # 파일명에서 날짜/시간 부분 추출 (PRO_VID_ 또는 VID_ 이후 부분)
+    if file_name.startswith('PRO_VID_'):
+        parts = file_name.split('PRO_VID_')[1].split('_')
+    else:  # VID_ 로 시작하는 경우
+        parts = file_name.split('VID_')[1].split('_')
+    
+    date_time_str = parts[0] + parts[1]  # YYYYMMDDHHMMSS 형식으로 조합
     date_time_obj = datetime.datetime.strptime(date_time_str, "%Y%m%d%H%M%S")
     
     # 타임스탬프 포맷 변환
@@ -21,7 +26,8 @@ def update_file_timestamp(file_path):
 
 def process_directory(directory):
     expanded_path = os.path.expanduser(directory)
-    for file_path in Path(expanded_path).glob("VID_*.mp4"):
+    # 대소문자 구분 없이 VID_ 와 PRO_VID_ 파일 검색
+    for file_path in Path(expanded_path).glob("*VID_*.[mM][pP]4"):
         update_file_timestamp(str(file_path))
 
 if __name__ == "__main__":
